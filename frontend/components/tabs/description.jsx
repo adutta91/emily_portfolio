65f071fde65b73ffae5dcc3a92bfc1ce68097013
Arjun Dutta <arjundutta91@gmail.com>
2016-09-01 20:07:22 -0700

    styling

--- frontend/components/tabs/description.jsx
+++ frontend/components/tabs/description.jsx
@@ -4,20 +4,9 @@ var Description = React.createClass({
   render: function() {
     return (
       <p className="description">
-        I am an amazing person who is modest, lovely, thoughtful, kind, empathetic,
-        funny, incredible, and all other manners of positive adjectives. I have
-        a boyfriend who loves me so incredibly much, and who thinks of me all the
-        time.
-        <br /> <br />
-        Other description stuff here. I just want to get a sense of the space.
-        You could talk about your education and background and what you're interested
-        in. You could also talk about your current position and the kind of stuff you
-        do. You could talk about your skills, interests, passions, etc.
-        <br /> <br />
-        Now is some concluding info. Perhaps talk more generally about you, what
-        you're looking to do now, in broad strokes. Maybe talk directly to the reader,
-        thank them for coming, or wish them a wonderful day. We can maybe even scan
-        your signature and paste it here.
+      Multilingual (English, Spanish and Arabic) team leader with experience in program management, global health issues, team building, customer relations, web content development, research, analysis, product innovation, promotion, organization development and mentoring.
+      Combines an extensive knowledge of international cultures with outstanding communication skills to ensure realization of organizational goals within the public and private sectors.
+      Effectively manages in diverse environments addressing key issues with efficient and positive solutions.
       </p>
     )
   }
